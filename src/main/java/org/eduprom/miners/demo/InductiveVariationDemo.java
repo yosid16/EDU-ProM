d7565d90dcd9c4149269ab373a4f3a2757001180
ydahari <yossi.dahari@intel.com>
2017-09-10 18:35:15 +0300

    bpm - inductive miner variation

--- src/main/java/org/eduprom/miners/demo/InductiveVariationDemo.java
+++ src/main/java/org/eduprom/miners/demo/InductiveVariationDemo.java
@@ -5,6 +5,8 @@ import org.deckfour.xes.model.XLog;
 import org.eduprom.miners.InductiveMiner;
 import org.processmining.log.algorithms.LowFrequencyFilterAlgorithm;
 import org.processmining.log.parameters.LowFrequencyFilterParameters;
+import org.processmining.plugins.petrinet.replayresult.PNRepResult;
+import org.processmining.ptconversions.pn.ProcessTree2Petrinet;
 
 public class InductiveVariationDemo extends InductiveMiner {
 
@@ -23,5 +25,16 @@ public class InductiveVariationDemo extends InductiveMiner {
     @Override
     public void evaluate() throws Exception {
         super.evaluate();
+        ProcessTree2Petrinet.PetrinetWithMarkings res = getDiscoveredPetriNet();
+
+        PNRepResult alignment = petrinetHelper.getAlignment(log, res.petrinet, res.initialMarking, res.finalMarking);
+        double fitness = Double.parseDouble(alignment.getInfo().get("Move-Model Fitness").toString());
+        logger.info(String.format("model fitness: %f", fitness));
+
+        //override how precision is measured, to use ETC plugin:
+        // "A fresh look at Precision in Process Conformance by Jorge Munoz-Gama and " +
+        //        "Josep Carmona, in Proceedings of Business Processes Management 2010 (BPM 2010)"
+        double precision = petrinetHelper.getPrecision(log, res.petrinet, alignment, res.initialMarking, res.finalMarking);
+        logger.info(String.format("model precision: %f", precision));
     }
 }
---
+++ src/main/java/org/eduprom/tasks/demo/DiscoverMinerVariation.java
@@ -0,0 +1,37 @@
+package org.eduprom.tasks.demo;
+
+import org.eduprom.miners.IMiner;
+import org.eduprom.miners.InductiveMiner;
+import org.eduprom.miners.demo.InductiveVariationDemo;
+
+import java.io.FileInputStream;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+
+public class DiscoverMinerVariation {
+	
+	private static final LogManager logManager = LogManager.getLogManager();
+	private static final Logger logger = Logger.getLogger(DiscoverMinerVariation.class.getName());
+	
+    public static void main(String[] args) throws Exception {
+
+        String filename = "EventLogs\\sample.xes";
+
+    	logManager.readConfiguration(new FileInputStream("./app.properties"));
+    	logger.info("started application");
+    	    	    	
+        try {
+        	IMiner miner = new InductiveVariationDemo(filename);
+        	miner.mine();
+        	miner.export();
+        	miner.evaluate();
+
+        } catch (Exception ex) {
+        	logger.log(Level.SEVERE, "exception when trying to train/evaluate the miner", ex);
+        }
+        
+        logger.info("ended application");
+    }
+}
